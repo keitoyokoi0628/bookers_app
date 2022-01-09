8f25f88a71e916f6d2d3a2c7a7d89bae2ae85a10
EC2 Default User <ec2-user@ip-172-31-92-109.ec2.internal>
2022-01-09 13:23:19 +0000

    編集機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get 'books' => 'books#index'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
 end
